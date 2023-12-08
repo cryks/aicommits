5258dad4354dea06bdeea0d1cf5bd09c7f5baee8
cryks <cryks0@gmail.com>
2023-12-09 00:36:30 +0900

    feat(utils/prompt): enhance guidelines for commit messages
    Updated the prompt generation function to include more
    detailed instructions on crafting commit messages. This
    aims to ensure a single-line format and the consolidation
    of multiple changes into one message.

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -2,11 +2,13 @@ export const generatePrompt = (maxLength: number) =>
 	[
 		"Generate a concise git commit message written in present tense for the following code diff with the given specifications below, aiming for the best result you can think of:",
 		"* It must always be written in English.",
+		"* It is absolutely essential to write in a single line.",
 		`* Commit message must be a maximum of ${maxLength} characters.`,
 		"* Please do not use words like 'Refactor' or 'Update'.",
 		"* Exclude anything unnecessary such as translation. Your entire response will be passed directly into git commit.",
 		"* Please do not output code blocks such as '```'.",
 		"* Please follow the Conventional Commits format for commit messages.",
+		"  * When there are multiple candidates, please consolidate them into one overarching message.",
 		"  * When bumping a module, please set scope: `build(deps)`.",
 		"  * It's unnecessary to include 'in ...' in the commit message. Instead, please include it in the scope of Conventional Commits.",
 		"    * For example, `feat: add ... in Dockerfile` should be `feat(Dockerfile): add ...`.",
@@ -14,20 +16,20 @@ export const generatePrompt = (maxLength: number) =>
 		"  * Please do not include file extensions in the scope.",
 		"    * For example, use `feat(index): add ...` instead of `feat(index.ts): add ...`.",
 		"Please generate the best commit message that has the above features and that you wouldn't be embarrassed to show to anyone. Thank you in advance.",
-	]
-		.filter(Boolean)
-		.join("\n");
+	].join("\n");
 
 export const generatePromptForBody = (maxLength: number) =>
 	[
 		"Next, please generate the body that will be the Conventional Commits Body.",
 		"This Body Message does not have the restrictions mentioned earlier.",
-		"However, please adhere to the following conditions.",
+		"However, please adhere to the following conditions:",
 		"* It must always be written in English.",
 		"* Please insert appropriate line breaks. About three lines is desirable.",
 		`* The maximum number of characters per line is ${maxLength}.`,
 		"* After inserting appropriate line breaks and after the Body Message, please write a brief summary in one line starting with the string `[SUMMARY]`, in *Japanese*.",
-		"Example:",
+		"  * This summary line is not used as a Body Message, so it doesn't matter how long it gets or if it exceeds the maximum number of characters per line.",
+		"",
+		"Example (The 'fix:' line is a sample commit message and should not be included in the body message):",
 		"  fix: prevent racing of requests",
 		"  ",
 		"  Introduce a request id and a reference to latest request. Dismiss",
@@ -35,6 +37,4 @@ export const generatePromptForBody = (maxLength: number) =>
 		"  ",
 		"  Remove timeouts which were used to mitigate the racing issue but are",
 		"  obsolete now.",
-	]
-		.filter(Boolean)
-		.join("\n");
+	].join("\n");
