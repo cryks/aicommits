e03ddbaf1fda20377094f3d0ff8400dec8145849
cryks <cryks0@gmail.com>
2023-12-12 14:13:51 +0900

    feat(utils/prompt): enhance commit guidelines

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -9,15 +9,17 @@ export const generatePrompt = (maxLength: number) =>
 		"* You should not use words like 'Refactor' or 'Update'.",
 		"* Exclude anything unnecessary such as translation. Your entire response will be passed directly into git commit.",
 		"* You should not output code blocks such as '```'.",
+		"* You must describe what will happen as a result of the changes, not the specific changes themselves.",
 		"* You should follow the Conventional Commits format for commit messages.",
 		"  * If the scope spans multiple areas, or there are multiple types, you should consolidate them into one type and scope.",
 		"  * When bumping a module, you should set scope: `build(deps)`.",
+		"  * You should use the `refactor` type for trivial corrections in the source code that do not affect operation, such as changing variable names, adjusting indents, swapping lines, etc.",
 		"  * It's unnecessary to include 'in ...' in the commit message. Instead, you should include it in the scope of Conventional Commits.",
 		"    * For example, `build: add ... in Dockerfile` should be `build(Dockerfile): add ...`.",
 		"    * For example, `fix(utils): fix typo in foobar.ts` should be `fix(utils/foobar): fix typo`.",
 		"  * You should not include file extensions in the scope.",
 		"    * For example, use `feat(index): add ...` instead of `feat(index.ts): add ...`.",
-		"Please generate the best commit message that has the above features and that you wouldn't be embarrassed to show to anyone. Thank you in advance.",
+		"Please generate the best commit message that has the above features. Take your time to revise and refine your message until you are confident it's the best expression you can produce. You should not be embarrassed to show this to anyone. Thank you in advance.",
 	].join("\n");
 
 export const generatePromptForBody = (maxLength: number) =>
